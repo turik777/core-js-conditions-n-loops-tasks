a2ceaeccd9010756793c54763603569e3da3301e
turik777 <52492191+turik777@users.noreply.github.com>
2024-11-29 05:04:31 +0300

    feat: complete isIsoscelesTriangle function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -92,8 +92,8 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  return a + b > c && b + c > a && c + a > b && (a === b || b === c || c === a);
 }
 
 /**
